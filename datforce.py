fe8aa40afd9e488848e34b57840f5d6350c1c04d
tuankhaibui <tuankhaibui.physics@gmail.com>
2024-06-18 20:10:33 +0900

    add a progress bar: update every 1 second, wait until 300s sync once. stop_button is added to waiting process.

--- datforce.py
+++ datforce.py
@@ -1,14 +1,36 @@
 #!/usr/bin/python3
-
 import getpass
 import pexpect
 import datetime
 import time
 import threading
-import keyboard
+
+def progress_bar(duration):
+    total_time = duration  # Total time in seconds
+    interval = 1  # Update interval for progress bar in seconds
+    elapsed_time = 0
+
+    while elapsed_time < total_time:
+        # Calculate progress percentage
+        progress = (elapsed_time / total_time) * 100
+        progress = min(progress, 100)  # Ensure progress does not exceed 100%
+
+        # Print the progress bar
+        bar_length = 100
+        filled_length = int(bar_length * progress / 100)
+        bar = '=' * filled_length + ' ' * (bar_length - filled_length)
+        print(f'\rWaiting: [{bar}] {elapsed_time}/{total_time}[s]', end='', flush=True)
+
+        # Wait for the update interval
+        time.sleep(interval)
+        elapsed_time += interval
+
+        if stop_event.is_set():
+            exit()
+
 
 hostname = input('Enter the hostname: ')
-username = getpass.getpass('Enter username: ')
+username = input('Enter username: ')
 password = getpass.getpass('Enter password: ')
 
 local_file_path = input('Enter local file path: ') #'/mnt/md0/qup/h5data'
@@ -16,12 +38,10 @@ remote_file_path = input('Enter remote file path: ') #'/group/cmb/kamioka/mKID/T
 
 print(f'Synchronize data from {local_file_path} in your computer to {remote_file_path} at {hostname}')
 
-# cmd = f"rsync -avz --delete -e ssh {local_file_path} {username}@{hostname}{remote_file_path}"
-
 
 def rsync_with_password(local_path, remote_path, password):
     # Construct the scp command
-    command = f'rsync -avz -e ssh {local_path} {remote_path}'
+    command = f'rsync -avr --progress --partial {local_path} {remote_path}'
     print(command)
     # command = f"scp {local_path} {remote_path}"
 
@@ -41,6 +61,7 @@ def rsync_with_password(local_path, remote_path, password):
 
 stop_event = threading.Event()
 
+
 def stop_button():
     while True:
         user_input = input("Enter 'stop' to stop: ")
@@ -52,9 +73,18 @@ thread = threading.Thread(target=stop_button)
 thread.daemon = True
 thread.start()
 
-# while True:
+
+def clear_screen():
+    # ANSI escape sequence for clearing screen
+    print('\033[H\033[J')
+
+
 while not stop_event.is_set():
+    #clear_screen()
     now = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    #print('[Enter "stop" to kill')
     print(f'{now}: Synchronizing data from {local_file_path} to {remote_file_path}...')
     rsync_with_password(local_file_path, f'{username}@{hostname}:{remote_file_path}', password)
-    time.sleep(1)
+    #time.sleep(300)
+    progress_bar(300)
+
